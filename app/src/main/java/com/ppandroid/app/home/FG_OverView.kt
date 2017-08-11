8ef3f885382e295a878afe20066adc9cfc990d1c
yeqinfu <990761790@qq.com>
2017-08-11 11:14:32 +0800

    主tab

--- app/src/main/java/com/ppandroid/app/home/FG_OverView.kt
+++ app/src/main/java/com/ppandroid/app/home/FG_OverView.kt
@@ -1,13 +1,23 @@
 package com.ppandroid.app.home
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.support.v4.view.PagerAdapter
+import android.support.v4.view.ViewPager
+import android.view.View
+import android.view.ViewGroup
 import com.ppandroid.app.R
+import com.ppandroid.app.bean.BN_Vertical
 import com.ppandroid.app.widget.wheelview.FitChartHalf
 import com.ppandroid.app.widget.wheelview.FitChartValue
 import com.ppandroid.im.base.FG_Base
 import kotlinx.android.synthetic.main.fg_over_view.*
-import kotlinx.android.synthetic.main.yellowchartview.*
 import org.jetbrains.anko.find
 import java.util.*
+
+
+
 
 /**
  * Created by yeqinfu on 2017/8/3.
@@ -16,27 +26,125 @@ class FG_OverView :FG_Base(){
     override fun fgRes():Int= R.layout.fg_over_view
 
     override fun afterViews() {
+        var list=ArrayList<BN_Vertical>()
+        for (i in 0..23){
+            var bn=BN_Vertical()
+            if (i%2==0){
+                if (i<10){
+                    bn.bottomText="0"+i
+                }else{
+                    bn.bottomText=""+i
+                }
+
+            }
+            bn.totalHeight=100f
+            var r=Random()
+            bn.realHeight=r.nextFloat()*100f
+            list.add(bn)
+        }
+
+        v_multiple_view.dataSet=list
+        btn_multi.setOnClickListener {
+            v_multiple_view.startAnim()
+
+        }
 
         val fitChart = view?.find<FitChartHalf>(R.id.fitChart)
         fitChart?.let {
             it.minValue=0f
             it.maxValue=100f
         }
-        btn_start_anim.setOnClickListener {
-            v_yellow_chart.startAnim()
-        }
-        add.setOnClickListener {
+
+        view?.postDelayed({
             val resources = resources
             val values = ArrayList<FitChartValue>()
-            values.add(FitChartValue(40f, resources.getColor(R.color.chart_value_1)))
+            values.add(FitChartValue(40f, resources.getColor(R.color.color_01)))
             fitChart?.setValues(values)
-        }
-        btn_start.setOnClickListener {
-            v_dount_view.startAnim()
-        }
+
+        },1000)
+
+
+
         btn_hp.setOnClickListener {
             v_hp.startAnim()
         }
+
+
+        val res = activity.resources
+        val img = BitmapFactory.decodeResource(res, R.drawable.icon_forward)
+        val matrix = Matrix()
+        matrix.postRotate(180f) /*翻转180度*/
+        val width = img.width
+        val height = img.height
+        val img_a = Bitmap.createBitmap(img, 0, 0, width, height, matrix, true)
+        iv_forward.setImageBitmap(img_a)
+        iv_forward.visibility=View.GONE
+
+
+        val lf = activity.layoutInflater
+        var view1 = lf.inflate(R.layout.item_yongdian, null)
+        var view2 = lf.inflate(R.layout.item_yongdian, null)
+        var view3 = lf.inflate(R.layout.item_yongdian, null)
+        viewList.add(view1)
+        viewList.add(view2)
+        viewList.add(view3)
+        view_pager.adapter=pagerAdapter
+        view_pager.addOnPageChangeListener(object :ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                if (position==0){
+                    iv_forward.visibility=View.GONE
+                }
+                if (position==viewList.size-1){
+                    iv_next.visibility=View.GONE
+                }
+                if (position>0&&position<viewList.size-1){
+                    iv_forward.visibility=View.VISIBLE
+                    iv_next.visibility=View.VISIBLE
+                }
+
+            }
+
+        })
     }
+
+    var viewList = ArrayList<View>()// 将要分页显示的View装入数组中
+
+    var pagerAdapter: PagerAdapter = object : PagerAdapter() {
+
+        override fun isViewFromObject(arg0: View, arg1: Any): Boolean {
+
+            return arg0 === arg1
+        }
+
+        override fun getCount(): Int {
+            return viewList.size
+        }
+
+        override fun destroyItem(container: ViewGroup, position: Int,
+                                 `object`: Any) {
+            container.removeView(viewList.get(position))
+
+        }
+
+        override fun getItemPosition(`object`: Any?): Int {
+
+            return super.getItemPosition(`object`)
+        }
+
+
+
+        override fun instantiateItem(container: ViewGroup, position: Int): Any {
+            container.addView(viewList[position])
+            return viewList[position]
+        }
+
+    }
+
 
 }
--- app/src/main/java/com/ppandroid/app/widget/MultipleVerticalView.java
+++ app/src/main/java/com/ppandroid/app/widget/MultipleVerticalView.java
@@ -1,7 +1,6 @@
 package com.ppandroid.app.widget;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,40 +8,36 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.ppandroid.app.R;
+import com.ppandroid.app.bean.BN_Vertical;
 import com.ppandroid.app.utils.DebugLog;
 
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by yeqinfu on 2017/8/9.
+ * Created by yeqinfu on 2017/8/10.
+ * 多条树状图
  */
 
-public class VerticalView extends View implements Runnable{
-    public VerticalView(Context context) {
+public class MultipleVerticalView extends View implements Runnable{
+    public MultipleVerticalView(Context context) {
         super(context);
         initSetting();
     }
 
-    public VerticalView(Context context, @Nullable AttributeSet attrs) {
+    public MultipleVerticalView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        readAttributes(attrs);
         initSetting();
     }
 
-
-
-    public VerticalView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public MultipleVerticalView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        readAttributes(attrs);
         initSetting();
     }
+    private void initSetting() {
+        strokeSize=40f;
 
-    private String topStr="";
-    private String bottomStr="";
-    private float totalHeight=0f;
-    private float realHeight=0f;
-    private float progress=0.0f;
-    private int lineColor=Color.WHITE;
+    }
 
     public float getProgress() {
         return progress;
@@ -52,31 +47,6 @@ public class VerticalView extends View implements Runnable{
         this.progress = progress;
     }
 
-    /**
-     * 属性读取
-     * @param attrs
-     */
-    private void readAttributes(AttributeSet attrs) {
-        if (attrs != null) {
-            TypedArray attributes = getContext()
-                    .getTheme().obtainStyledAttributes(attrs, R.styleable.verticalview, 0, 0);
-            totalHeight = attributes
-                    .getFloat(R.styleable.verticalview_totalHeight, 0f);
-            realHeight = attributes
-                    .getFloat(R.styleable.verticalview_realHeight, 0f);
-            topStr = attributes
-                    .getString(R.styleable.verticalview_topText);
-            bottomStr = attributes
-                    .getString(R.styleable.verticalview_bottomText);
-            lineColor=attributes.getColor(R.styleable.verticalview_lineColor,Color.WHITE);
-            attributes.recycle();
-        }
-    }
-
-    private void initSetting() {
-        strokeSize=40f;
-
-    }
     private float strokeSize;
     private Paint getPaint() {
         if (!isInEditMode()) {
@@ -94,34 +64,65 @@ public class VerticalView extends View implements Runnable{
     }
     private Paint getPicPaint(){
         Paint paint = getPaint();
-        paint.setColor(lineColor);
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
         paint.setStrokeWidth(strokeSize);
+
         return paint;
+    }
+
+
+    private List<BN_Vertical> dataSet = new ArrayList<>();
+
+    public List<BN_Vertical> getDataSet() {
+        return dataSet;
+    }
+
+    public void setDataSet(List<BN_Vertical> dataSet) {
+        this.dataSet = dataSet;
+
+    }
+
+    private float progress = 0.0f;
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (!isInEditMode()){
+            strokeSize=getWidth()/(dataSet.size()*2);
+            DebugLog.d("++++++++++strokeSize"+strokeSize);
+        }
+
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+
         Paint p=getTextPaint();
         Paint.FontMetrics fontMetrics = p.getFontMetrics();
         float top = fontMetrics.top;//为基线到字体上边框的距离,即上图中的top
         float bottom = fontMetrics.bottom;//为基线到字体下边框的距离,即上图中的bottom
         int baseLineY = (int) (bottom-top);//基线中间点的y轴计算公式
 
+        for (int i=0;i<dataSet.size();i++) {
+            BN_Vertical item=dataSet.get(i);
+            float itemProgress=progress*item.getRealHeight()/item.getTotalHeight();
+            float begin=(i+0.5f)*getWidth()/dataSet.size();
+            float h=itemProgress*(getHeight()-baseLineY*2.5f);
+            canvas.drawText(item.getTopText(),begin,getHeight()-baseLineY*1.5f-h,p);
+            canvas.drawText(item.getBottomText(),begin,getHeight(),p);
+            Paint p2=getPicPaint();
+            float v1=getHeight()-h-baseLineY;
+            float v2=getHeight()-baseLineY;
+            if (v1<v2){
+                canvas.drawLine(begin,v1,begin,v2,p2);
+            }
 
-        float begin=getWidth()/2;
-        float h=progress*(getHeight()-baseLineY*2.5f);
-        canvas.drawText(topStr,begin,getHeight()-baseLineY*1.5f-h,p);
-        canvas.drawText(bottomStr,begin,getHeight(),p);
-        Paint p2=getPicPaint();
-
-
-        float v1=getHeight()-h-baseLineY;
-        float v2=getHeight()-baseLineY;
-        if (v1<v2){
-            canvas.drawLine(begin,v1,begin,v2,p2);
         }
+
     }
+
+
     @Override
     public void run() {
         // TODO Auto-generated method stub
@@ -137,9 +138,7 @@ public class VerticalView extends View implements Runnable{
         for (int i=1;i<=100;i++){
             try {
                 Thread.sleep(5);
-                float t=realHeight/totalHeight;
-                DebugLog.d("=============="+t);
-                setProgress((float) i*t/100);
+                setProgress((float) i/100);
                 postInvalidate();
             } catch (InterruptedException e) {
                 e.printStackTrace();
