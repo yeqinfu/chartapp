6ea482acd82c7d3e254fee2493a7db6017d581a5
yeqinfu <990761790@qq.com>
2017-08-14 09:01:19 +0800

    登录逻辑

---
+++ app/src/main/java/com/ppandroid/app/AC_Welcome.kt
@@ -0,0 +1,21 @@
+package com.ppandroid.app
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.ppandroid.app.utils.info.Utils_UserInfo
+
+class AC_Welcome : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ac__welcome)
+        var it=Intent()
+        if (Utils_UserInfo.isLogined(this)){
+            it.setClass(this,AC_Main::class.java)
+        }else{
+            it.setClass(this,AC_Login::class.java)
+        }
+        startActivity(it)
+    }
+}
