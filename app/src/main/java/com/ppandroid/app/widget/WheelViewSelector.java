bdc1eb70d241ea603cf80ca0a9af03fb91cfe980
yeqinfu <990761790@qq.com>
2017-09-18 08:55:18 +0800

    文案修改

--- app/src/main/java/com/ppandroid/app/widget/WheelViewSelector.java
+++ app/src/main/java/com/ppandroid/app/widget/WheelViewSelector.java
@@ -2,12 +2,10 @@ package com.ppandroid.app.widget;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorFilter;
 import android.graphics.Paint;
-import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
@@ -19,6 +17,7 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import com.ppandroid.app.R;
 import com.ppandroid.app.utils.DebugLog;
 
 import java.util.ArrayList;
@@ -189,14 +188,22 @@ public class WheelViewSelector extends ScrollView {
 
     int itemHeight = 0;
 
-    private TextView createView() {
+
+
+    private TextView createView(String item) {
         TextView tv = new TextView(context);
         tv.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
         tv.setSingleLine(true);
-        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+        tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);
         tv.setGravity(Gravity.CENTER);
         int padding = dip2px(15);
         tv.setPadding(padding, padding, padding, padding);
+        tv.setText(item);
+        if(!TextUtils.isEmpty(item)){
+            Drawable drawable=getResources().getDrawable(R.drawable.zuzt);
+            drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
+            tv.setCompoundDrawables(null, drawable, null, null);
+        }
         if (0 == itemHeight) {
             itemHeight = getViewMeasuredHeight(tv);
             Log.d(TAG, "itemHeight: " + itemHeight);
@@ -205,12 +212,6 @@ public class WheelViewSelector extends ScrollView {
             lp.gravity=Gravity.CENTER;
             this.setLayoutParams(new LinearLayout.LayoutParams(lp.width, ViewGroup.LayoutParams.MATCH_PARENT));
         }
-        return tv;
-    }
-
-    private TextView createView(String item) {
-        TextView tv = createView();
-        tv.setText(item);
         return tv;
     }
 
@@ -324,47 +325,7 @@ public class WheelViewSelector extends ScrollView {
     Paint paint;
     int viewWidth;
 
-    @Override
-    public void setBackgroundDrawable(Drawable background) {
 
-        if (viewWidth == 0) {
-            viewWidth = ((Activity) context).getWindowManager().getDefaultDisplay().getWidth();
-            Log.d(TAG, "viewWidth: " + viewWidth);
-        }
-
-        if (null == paint) {
-            paint = new Paint();
-            paint.setColor(Color.parseColor("#83cde6"));
-            paint.setStrokeWidth(dip2px(1f));
-        }
-
-        background = new Drawable() {
-            @Override
-            public void draw(Canvas canvas) {
-                canvas.drawLine(viewWidth * 1 / 6, obtainSelectedAreaBorder()[0], viewWidth * 5 / 6, obtainSelectedAreaBorder()[0], paint);
-                canvas.drawLine(viewWidth * 1 / 6, obtainSelectedAreaBorder()[1], viewWidth * 5 / 6, obtainSelectedAreaBorder()[1], paint);
-            }
-
-            @Override
-            public void setAlpha(int alpha) {
-
-            }
-
-            @Override
-            public void setColorFilter(ColorFilter cf) {
-
-            }
-
-            @Override
-            public int getOpacity() {
-                return PixelFormat.UNKNOWN;
-            }
-        };
-
-
-        super.setBackgroundDrawable(background);
-
-    }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
@@ -381,8 +342,17 @@ public class WheelViewSelector extends ScrollView {
         super.onSizeChanged(w, h, oldw, oldh);
         Log.d(TAG, "w: " + w + ", h: " + h + ", oldw: " + oldw + ", oldh: " + oldh);
         viewWidth = w;
-        setBackgroundDrawable(null);
 
+        if (viewWidth == 0) {
+            viewWidth = ((Activity) context).getWindowManager().getDefaultDisplay().getWidth();
+            Log.d(TAG, "viewWidth: " + viewWidth);
+        }
+
+        if (null == paint) {
+            paint = new Paint();
+            paint.setColor(Color.parseColor("#83cde6"));
+            paint.setStrokeWidth(dip2px(1f));
+        }
     }
 
     /**
