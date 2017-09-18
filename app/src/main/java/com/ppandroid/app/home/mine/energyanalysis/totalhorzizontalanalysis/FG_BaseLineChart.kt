df34cc7af701910a718aca18addf14efa0f131a6
yeqinfu <990761790@qq.com>
2017-09-18 17:30:46 +0800

    折线图

--- app/src/main/java/com/ppandroid/app/home/mine/energyanalysis/totalhorzizontalanalysis/FG_BaseLineChart.kt
+++ app/src/main/java/com/ppandroid/app/home/mine/energyanalysis/totalhorzizontalanalysis/FG_BaseLineChart.kt
@@ -2,13 +2,32 @@ package com.ppandroid.app.home.mine.energyanalysis.totalhorzizontalanalysis
 
 import com.ppandroid.app.R
 import com.ppandroid.im.base.FG_Base
+import kotlinx.android.synthetic.main.fg_base_line_chart.*
+import java.util.*
 
 /**
  * Created by yeqinfu on 2017/9/18.
  */
 open class FG_BaseLineChart:FG_Base(){
     override fun fgRes(): Int= R.layout.fg_base_line_chart
-
+    //x轴坐标对应的数据
+    private val xValue = ArrayList<String>()
+    //y轴坐标对应的数据
+    private val yValue = ArrayList<Int>()
+    //折线对应的数据
+    private val value = HashMap<String, Int>()
     override fun afterViews() {
+        xValue.clear()
+        yValue.clear()
+        value.clear()
+        for (i in 0..11) {
+            xValue.add((i + 1).toString())
+            value.put((i + 1).toString(), (Math.random() * 181 + 60).toInt())//60--240
+        }
+
+        for (i in 0..5) {
+            yValue.add(i * 60)
+        }
+        chartview.setValue(value, xValue, yValue)
     }
 }
---
+++ app/src/main/java/com/ppandroid/app/widget/linechart/BaseLineChart.java
@@ -0,0 +1,34 @@
+package com.ppandroid.app.widget.linechart;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+
+import com.ppandroid.app.widget.graphical.view.GraphicalView;
+
+/**
+ * Created by yeqinfu on 2017/9/18.
+ */
+
+public class BaseLineChart extends GraphicalView {
+    public BaseLineChart(Context context) {
+        super(context);
+    }
+
+    public BaseLineChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BaseLineChart(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    //
+    private double maxValue=0;
+
+    @Override
+    public void render(Canvas canvas) {
+
+
+    }
+}
